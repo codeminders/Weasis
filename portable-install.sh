87c44fb8f95ec7a911f826a03c0d02fc0ff7650f
Egor Kulbachka <ekulbachka@codeminders.com>
2018-10-25 21:31:56 +0200

    [GHCAD-73] use pack200 to compress portable distribution

--- portable-install.sh
+++ portable-install.sh
@@ -4,4 +4,4 @@ mvn clean install
 mvn install:install-file -Dfile=./google-demo-client/target/google-demo-service-0.0.1-SNAPSHOT-jar-with-dependencies.jar -DgroupId=com.codeminders -DartifactId=google-demo-service-jar-with-dependencies -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar
 
 cd ./weasis-distributions
-mvn clean package -Dportable=true
+mvn clean package -Dportable=true -P pack200
